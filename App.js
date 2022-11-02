743e4c1bdde6ad963248423334a297c3d0be5683
Gowtham D <gowthamg1412@gmail.com>
2022-11-02 15:56:20 +0530

    First Commit

---
+++ App.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+export default App;
+
+function App() {
+  return(
+    <View style={{alignContent:'center',alignItems:'center',flex:1}}>
+      <Text>
+        Hello
+      </Text>
+    </View>
+  );
+}
